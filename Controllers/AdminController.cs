259c8d6931664e45e169eca5cfca402b615cd859
MOHIT SANWAL <mohitsanwalneha@gmail.com>
2022-07-15 18:02:03 +0530

    add bootstrap scaffolding creating controller and api controller

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using BlackStoneMovies.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +10,27 @@ namespace BlackStoneMovies.Controllers
 {
     public class AdminController : Controller
     {
+        HttpClient client = new HttpClient();
+
         // GET: Admin
-        public ActionResult Index()
+        public ActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string Email,string Password)
+        {
+            client.BaseAddress = new Uri("http://localhost:64815/api/AdminApi");
+            var response = client.GetAsync("AdminApi");
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Role>();
+                display.Wait();
+                Session["Role"] = display.Result.URole;
+                Session["Name"]
+            }
             return View();
         }
     }
